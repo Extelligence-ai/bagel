d1b78aa0d6ee0c8f1c3b1761cce866e71b206112
Shouheng Yi <shouhengyi@gmail.com>
2025-09-13 11:32:34 -0700

    Added ArduPilot TopicRegistry

---
+++ src/topic/ardupilot/registry.py
@@ -0,0 +1,53 @@
+"""A topic registry for ArduPilot Dataflash logs."""
+
+import pyarrow as pa
+from pymavlink import DFReader
+
+from src.topic import base
+from src.topic.ardupilot import schema
+
+
+class TopicRegistry(base.TopicRegistry):
+    """A topic registry for ArduPilot Dataflash logs."""
+
+    def available_topics(self, data_source: DFReader.DFReader) -> list[str]:
+        """Return a list of available format names."""
+        fmt_names = []
+        for fmt_id, cnt in enumerate(data_source.counts):
+            if cnt > 0 and fmt_id in data_source.id_to_name:
+                fmt_names.append(data_source.id_to_name[fmt_id])
+        return sorted(fmt_names)
+
+    def native_type_name(self, topic: str, data_source: DFReader.DFReader) -> str:
+        """Return the native type name for the given format name."""
+        if topic not in self.available_topics(data_source):
+            raise base.TopicNotFoundError(topic)
+
+        return str(data_source.name_to_id[topic])
+
+    def message_count(self, topic: str, data_source: DFReader.DFReader) -> int:
+        """Return the number of messages for the given format name."""
+        if topic not in self.available_topics(data_source):
+            raise base.TopicNotFoundError(topic)
+
+        fmt_id = data_source.name_to_id[topic]
+        return data_source.counts[fmt_id]
+
+    def struct(self, topic: str, data_source: DFReader.DFReader) -> pa.StructType:
+        """Return the PyArrow StructType for the given format name."""
+        if topic not in self.available_topics(data_source):
+            raise base.TopicNotFoundError(topic)
+
+        fmt_id = data_source.name_to_id[topic]
+        fmt = data_source.formats[fmt_id]
+        return schema.to_pa_struct(fmt, data_source.metadata)
+
+    def describe(self, topic: str, data_source: DFReader.DFReader) -> str | None:
+        """Return a human-readable description of the given format name."""
+        if topic not in self.available_topics(data_source):
+            raise base.TopicNotFoundError(topic)
+
+        fmt_id = data_source.name_to_id[topic]
+        fmt = data_source.formats[fmt_id]
+        node = data_source.metadata.metadata_tree().get(fmt.name)
+        return node.description.text if node is not None and node.description else None
