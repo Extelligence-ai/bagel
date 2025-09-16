d0941d7b2265bb0f2ae217aad8797cdab1af597c
Shouheng Yi <shouhengyi@gmail.com>
2025-09-16 11:11:49 -0700

    Added Betaflight MessageDataset

---
+++ src/message/betaflight/bfl.py
@@ -0,0 +1,7 @@
+"""A message dataset for Betaflight Blackbox logs."""
+
+from src.message.betaflight import bbl
+
+
+class MessageDataset(bbl.MessageDataset):
+    """A message dataset for Betaflight Blackbox logs."""
